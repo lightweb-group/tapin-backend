761c98da6073833e5d36b0f6fc2b2cc93f4e6ecb
mahatohariom <mahatohariom31@gmail.com>
2025-05-02 08:05:16 +0545

    merchant table has been done

---
+++ prisma/migrations/20250502022001_create_the_mercahnt_table_and_the_realtion_with_user/migration.sql
@@ -0,0 +1,20 @@
+-- AlterTable
+ALTER TABLE "customers" ADD COLUMN     "merchantId" TEXT;
+
+-- CreateTable
+CREATE TABLE "merchants" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "address" TEXT,
+    "pointsPerVisit" INTEGER NOT NULL DEFAULT 10,
+    "pointsPerDollar" DOUBLE PRECISION,
+    "welcomeBonus" INTEGER NOT NULL DEFAULT 0,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "merchants_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "customers" ADD CONSTRAINT "customers_merchantId_fkey" FOREIGN KEY ("merchantId") REFERENCES "merchants"("id") ON DELETE SET NULL ON UPDATE CASCADE;
